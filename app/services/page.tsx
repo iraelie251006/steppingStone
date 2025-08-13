ef4fec1bb764b8d3f9c7ccb3c1065486da56896d
Elie <niyubwayoiraelie5777@gmail.com>
2025-08-13 12:54:51 +0200

    Added Services demo page

---
+++ app/services/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <section>
+        <h1 className='font-bold text-4xl flex justify-center'>Our Services</h1>
+    </section>
+  )
+}
+
+export default page
