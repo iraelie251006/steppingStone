766b098c621e534c8d470e1bed3c868d74fb5c95
Elie <niyubwayoiraelie5777@gmail.com>
2025-08-13 12:51:55 +0200

    Added Team demo page

---
+++ app/team/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <section>
+        <h1 className='font-bold text-4xl flex justify-center'>Team of Stepping stone</h1>
+    </section>
+  )
+}
+
+export default page
