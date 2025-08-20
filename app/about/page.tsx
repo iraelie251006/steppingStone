e2f087535c318e0358ce6d76323fc400cd1e7b46
Elie <niyubwayoiraelie5777@gmail.com>
2025-08-20 18:23:10 +0200

    Added sidebar links to desktop navbar and adjusted mobile navigation visibility and added about page

---
+++ app/about/page.tsx
@@ -0,0 +1,10 @@
+
+const page = () => {
+  return (
+    <section>
+        <h1 className='font-bold text-4xl flex justify-center'>About Stepping stone</h1>
+    </section>
+  )
+}
+
+export default page
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,12 +1,21 @@
 import Image from 'next/image'
 import React from 'react'
 import MobileNavigation from './MobileNavigation'
+import { sidebarLinks } from '@/lib/navLinks'
+import Link from 'next/link'
 
 const Navbar = () => {
   return (
-    <nav className='flex justify-between border-b-2 px-4'>
+    <nav className='flex justify-between border-b-2 max-sm:px-4 px-10'>
         <div className=''>
             <Image src="/SteppingStone logo.png" alt='stepping stone logo' width={300} height={200} />
+        </div>
+        <div className='flex items-center max-sm:hidden'>
+          {sidebarLinks.map(({label, route}) => (
+            <Link href={route} key={label} className='font-semibold px-4'>
+              {label}
+            </Link>
+          ))}
         </div>
         <MobileNavigation />
     </nav>
